ea20e5305c61e1dc5bbb6b7d3285b9db6d4f4902
alexmartin1124 <alexseth1124@gmail.com>
2017-03-14 12:28:37 -0400

    configures router and adds js and css assets

--- src/main.js
+++ src/main.js
@@ -1,15 +1,25 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import App from './App'
-import router from './router'
+import AppView from './App'
+import VueRouter from 'vue-router'
+import routes from './routes'
+
+Vue.use(VueRouter)
 
 Vue.config.productionTip = false
+
+var router = new VueRouter({
+  routes: routes,
+  mode: 'history',
+  scrollBehavior: function (to, from, savedPosition) {
+    return savedPosition || { x: 0, y: 0 }
+  }
+})
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
-  router,
-  template: '<App/>',
-  components: { App }
+  router: router,
+  render: h => h(AppView)
 })
---
+++ src/routes.js
@@ -0,0 +1,8 @@
+import Home from './components/Home.vue'
+
+const routes = [{
+  path: '/',
+  component: Home
+}]
+
+export default routes
